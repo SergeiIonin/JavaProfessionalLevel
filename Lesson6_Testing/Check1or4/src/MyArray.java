6e73b58e8743b8ad603c7ce06083ac39a18e411c
Sergey Ionin <36512667+senin26@users.noreply.github.com>
2018-12-11 18:41:09 +0300

    Update MyArray.java
    Parametrization was added.

--- Lesson6_Testing/Check1or4/src/MyArray.java
+++ Lesson6_Testing/Check1or4/src/MyArray.java
@@ -1,21 +1,17 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class MyArray {
 
-    private Integer [] array;
+    private Integer[] array;
     private List<Integer> list;
 
-    public MyArray(Integer... args) {
+    public boolean contains1or4(Integer[] args){
         this.array = new Integer[args.length];
         for (int i = 0; i < args.length; i++) {
             this.array[i] = args[i];
         }
         this.list= Arrays.asList(this.array);
-    }
-
-    public boolean contains1or4(){
         if (this.list.contains(1)) return true;
         else if (this.list.contains(4)) return true;
         else return false;
