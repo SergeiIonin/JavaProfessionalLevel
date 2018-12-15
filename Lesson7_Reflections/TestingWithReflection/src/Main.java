02cd1799bff9163cdf58d7084844234c366a2981
Sergey Ionin <36512667+senin26@users.noreply.github.com>
2018-12-15 09:20:49 +0300

    Update Main.java

--- Lesson7_Reflections/TestingWithReflection/src/Main.java
+++ Lesson7_Reflections/TestingWithReflection/src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        TestClass testClass = new TestClass();
         TestHandler testHandler = new TestHandler();
-        testHandler.start(testClass);
+        testHandler.start("TestClass");
     }
 
 }
-
